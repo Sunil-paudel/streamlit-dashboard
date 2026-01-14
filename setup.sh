98fd84a7df4f43d5a2de8548c904750e215498ae
Sunil chandra paudel <sunny@Sunils-MacBook-Air.local>
2026-01-15 09:35:01 +1100

    Remove unused legacy files

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mkdir -p ~/.streamlit/
-echo "[server]
-enableCORS = false
-enableXsrfProtection = false
-headless = true
-[browser]
-gatherUsageStats = false
-" > ~/.streamlit/config.toml
